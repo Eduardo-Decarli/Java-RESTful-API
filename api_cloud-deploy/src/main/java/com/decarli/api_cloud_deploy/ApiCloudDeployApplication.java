37c6d52e7368cc56fddb06d11cab087fdfe1cf9d
Eduardo-Decarli <decarliedu@gmail.com>
2025-02-18 12:52:14 -0300

    feat: import inicial project form

---
+++ api_cloud-deploy/src/main/java/com/decarli/api_cloud_deploy/ApiCloudDeployApplication.java
@@ -0,0 +1,13 @@
+package com.decarli.api_cloud_deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCloudDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCloudDeployApplication.class, args);
+	}
+
+}
---
+++ api_cloud-deploy/src/test/java/com/decarli/api_cloud_deploy/ApiCloudDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decarli.api_cloud_deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCloudDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
